2ebf615371857f6a1e944c79bd94e20dc5801ce1
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-02-11 22:10:49 +0300

    Completed labs

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        match_words =[]
+        array.each do |name|
+            if @word.chars.sort == name.chars.sort
+                match_words << name
+            end
+        end
+        match_words
+    end
+end
